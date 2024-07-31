72ffbb0366a3be7593f259616567cb61b36e0afa
Arthur Axiotes <axiotesarthur@gmail.com>
2024-07-31 18:27:33 -0300

    feat: header desktop and mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list'
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router, ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -17,10 +18,31 @@ import { RouterOutlet, RouterLink } from '@angular/router';
     MatListModule,
     RouterOutlet,
     RouterLink,
+    CommonModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public navOptions: { route: string, title: string, isActive: boolean }[] = [
+    { route: "/home", title: "Início", isActive: false },
+    { route: "/about-us", title: "Sobre", isActive: false },
+    { route: "/news", title: "Notícias", isActive: false },
+    { route: "/ethical-values", title: "Valores", isActive: false },
+    { route: "/contact", title: "Contatos", isActive: false },
+  ]
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.chanceActiveRoute("/home");
+  }
+
+  public chanceActiveRoute(route: string) {
+    this.navOptions.filter((option) => option.route === route).map((option) => option.isActive = true);
+    this.navOptions.filter((option) => option.route !== route).map((option) => option.isActive = false);
+  }
 }
