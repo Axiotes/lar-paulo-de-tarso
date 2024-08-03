56200519d09a2d0bbbfe8abe749ba0b04102582f
Arthur Axiotes <axiotesarthur@gmail.com>
2024-08-03 17:43:49 -0300

    feat: news page desktop and mobile

---
+++ src/app/components/card-new/card-new.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-new.component.html',
+  styleUrl: './card-new.component.scss'
+})
+export class CardNewComponent {
+  @Input({ required: true }) public url: string = ""
+  @Input({ required: true }) public message: string = ""
+}
--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CardNewComponent } from '../../components/card-new/card-new.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [],
+  imports: [
+    CardNewComponent,
+    CommonModule,
+  ],
   templateUrl: './news.component.html',
   styleUrl: './news.component.scss'
 })
 export class NewsComponent {
-
+  public news: { url: string, message: string }[] = [
+    { url: "/assets/images/news01.png", message: "Um dia especial" },
+    { url: "/assets/images/news02.png", message: "Lista de casamento" },
+    { url: "/assets/images/news03.png", message: "Sábado da alegria" },
+  ]
 }
