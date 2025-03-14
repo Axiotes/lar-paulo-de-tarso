126ca3e08c4b1a54932f23081600c8c29b47fe53
Arthur Axiotes <axiotesarthur@gmail.com>
2025-03-14 14:24:40 -0300

    fix: subtituir imagens por icons na tela de contatos

--- src/app/components/social-medias/social-medias.component.ts
+++ src/app/components/social-medias/social-medias.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
 
 @Component({
   selector: 'app-social-medias',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './social-medias.component.html',
   styleUrl: './social-medias.component.scss'
 })
 export class SocialMediasComponent {
-  @Input({ required: true }) public url: string = "";
-  @Input({ required: true }) public socialMedia: string = "";
+  @Input({ required: true }) public icon!: LucideIconData;
+  @Input({ required: true }) public socialMedia!: string;
 }
